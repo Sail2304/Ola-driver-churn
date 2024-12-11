40a00a26db122b9bbe04789931e10cd5d66150b5
Sail <gangan.sail@gmail.com>
2024-12-11 14:51:04 +0530

    data validation stage added

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from src.OLAChurnPred import logger
 from src.OLAChurnPred.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.OLAChurnPred.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.initiate_data_ingestion()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.initiate_data_validation()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
---
+++ src/OLAChurnPred/pipeline/data_validation_pipeline.py
@@ -0,0 +1,28 @@
+from src.OLAChurnPred.config.configuration import ConfigurationManager
+from src.OLAChurnPred.components.data_validation import DataValidation
+from src.OLAChurnPred import logger
+
+
+STAGE_NAME="Data validation Stage"
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_data_validation(self):
+        config=ConfigurationManager()
+        data_validation_config=config.get_data_validation_config()
+        data_validation=DataValidation(config=data_validation_config)
+        data_validation.validate_all_columns()
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataValidationTrainingPipeline()
+        obj.initiate_data_ingestion()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
