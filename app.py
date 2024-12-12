371a50474fb7b82a81b976606b1b90befc2b907e
Sail <gangan.sail@gmail.com>
2024-12-13 00:27:51 +0530

    flask api created

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+import os
+import pandas as pd
+from src.OLAChurnPred.pipeline.prediction_pipeline import churnpredict
+
+app = Flask(__name__)
+
+
+@app.route("/train", methods=['GET'])
+def training():
+    os.system("python main.py")
+    return "Training Successful!"
+
+#defining the endpoint which will make the prediction
+@app.route("/predict", methods=['POST'])
+def prediction():
+    """ Returns loan application status using ML model
+    """
+    # Age,Gender,Education,Income,Joining_Designation,Grade,Total_Business_Value,Last_Quarterly_Rating,Quarterly_Rating_Increased,
+    # Salary_Increased,Target,C10,C11,C12,C13,C14,C15,C16,C17,C18,C19,C2,C20,C21,C22,C23,C24,C25,C26,C27,C28,C29,C3,C4,C5,C6,C7,C8,C9
+
+    churn_req = request.get_json()
+    churn_req = dict(churn_req)
+    data = pd.DataFrame(churn_req, index=[0])
+    res = churnpredict(data)
+    print(res)
+
+    return {"response": int(res)}
---
+++ src/OLAChurnPred/pipeline/prediction_pipeline.py
@@ -0,0 +1,14 @@
+import joblib
+from pathlib import Path
+import pandas as pd
+
+def churnpredict(data):
+    ohe = joblib.load(Path('artifacts/data_transformation/ohencoder.joblib'))
+    model = joblib.load(Path('artifacts/model_trainer/model.joblib'))
+
+    enc = ohe.transform(data['City'].values.reshape(1,-1))
+    enc_df = pd.DataFrame(enc.toarray(), columns=ohe.categories_[0][1:])
+    data = pd.concat([data,enc_df], axis=1)
+    data = data.drop(columns=['City'])
+    res = model.predict(data.loc[0, :].to_numpy().reshape(1,-1))
+    return res
