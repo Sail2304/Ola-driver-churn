b50fd7efbcefa912b54304a2fce576f710c93d6a
Sail <gangan.sail@gmail.com>
2024-12-15 21:51:21 +0530

    added docker file

--- app.py
+++ app.py
@@ -26,3 +26,8 @@ def prediction():
     print(res)
 
     return {"response": int(res)}
+
+if __name__=="__main__":
+    # app.run(host="0.0.0.0",port=8080)        
+    app.run(host='0.0.0.0', port=8080)        
+
