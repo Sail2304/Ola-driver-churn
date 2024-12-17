3873baabb4a653604c1e249dc5b58122515b59b8
Sail <gangan.sail@gmail.com>
2024-12-17 18:25:59 +0530

    heroku deployment

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def prediction():
 
     return {"response": int(res)}
 
-if __name__=="__main__":
-    # app.run(host="0.0.0.0",port=8080)        
-    app.run(host='0.0.0.0', port=8080)        
+# if __name__=="__main__":
+#     # app.run(host="0.0.0.0",port=8080)        
+#     app.run(host='0.0.0.0', port=8080)        
 
